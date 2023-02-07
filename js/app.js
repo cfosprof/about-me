a3c6be5e3e9aa8dbe6280e54409c9c7c1aa6b3eb
Christopher Foster <cfosprof@gmail.com>
2023-02-07 14:12:28 -0800

    Putting the skeleton of this course up

---
+++ js/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// TODO: Get useres name and alert greet back to the user
+let userName = prompt('What is your name?')
+
+alert(`Welcome to my site ${userName}, let's play a guessing game.. please answer yes/y or no/n`);
+//todo prompt 5 yes/no or y/n questions when they are correct or in correct
+
+// if (!validAnswers.includes(questionOneGuess.toLowerCase())) {
+//   alert(`You answered ${questionOneGuess}, and it is not a valid answer. Please enter a valid yes/y no/n answer`);
+// } else if(!rightAnswer.includes(questionOneGuess.toLowerCase())) {
+//   // console.log('That is incorrect');
+//   alert('That is incorrect');
+// } else {
+//   // console.log('Great Guess');
+//   alert('Great Guess');
+// }
+
+function evalAnswer(question, rightAnswer) {
+  let userAnswer = prompt(question)
+  let validAnswer = ['yes', 'y', 'no', 'n',];
+  // let rightAnswer = ['yes', 'y'];
+  while (!validAnswer.includes(userAnswer.toLowerCase())) {
+    userAnswer = prompt(`You answered ${userAnswer}, and it is not a valid answer. Please enter a valid yes/y no/n answer`);
+    // console.log(`User answered ${userAnswer}`)
+  }
+  if (!rightAnswer.includes(userAnswer.toLowerCase())) {
+    // console.log(`${userAnswer} is incorrect`);
+    alert(`${userAnswer} is incorrect`);
+  } else {
+    // console.log('Great Guess');
+    alert('Great Guess');
+  }
+}
+
+evalAnswer('Do I have a sister', ['yes', 'y']);
+
+evalAnswer('Am I cool', ['no', 'n']);
+
+evalAnswer('Is the earth flat?', ['no', 'n']);
+
+evalAnswer('Are prompt and alert boxes annoying?', ['yes', 'y']);
+
+evalAnswer('Should I have skipped lunch?', ['no', 'n']);
+
+// //todo: give them afinal message with their name in the alert
+
+alert(`Thanks for playing ${userName}!`);
