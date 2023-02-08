7f96c2776dd244fec1340731776e30c77152c1e4
Christopher Foster <cfosprof@gmail.com>
2023-02-08 11:52:54 -0800

    message updating evalanswer to handle a for loop where we limit the amount of guesses a user is given.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,50 @@
 'use strict';
 
 // TODO: Get useres name and alert greet back to the user
-let userName = prompt('What is your name?')
+let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}, let's play a guessing game.. please answer yes/y or no/n`);
 //todo prompt 5 yes/no or y/n questions when they are correct or in correct
+
+// let score = 0;
+// function evalAnswer(question, rightAnswer) {
+//   let userAnswer = prompt(question);
+//   let validAnswer = ['yes', 'y', 'no', 'n',];
+//   while (!validAnswer.includes(userAnswer.toLowerCase())) {
+//     userAnswer = prompt(`You answered ${userAnswer}, and it is not a valid answer. Please enter a valid yes/y no/n answer`);
+//     // console.log(`User answered ${userAnswer}`)
+//   }
+//   if (!rightAnswer.includes(userAnswer.toLowerCase())) {
+//     // console.log(`${userAnswer} is incorrect`);
+//     alert(`${userAnswer} is incorrect`);
+//   } else {
+//     // console.log('Great Guess');
+//     alert('Great Guess');
+//     score++;
+//   };
+//   // console.log(score);
+// }
 
 let score = 0;
 function evalAnswer(question, rightAnswer) {
   let userAnswer = prompt(question);
   let validAnswer = ['yes', 'y', 'no', 'n',];
-  // let rightAnswer = ['yes', 'y'];
   while (!validAnswer.includes(userAnswer.toLowerCase())) {
     userAnswer = prompt(`You answered ${userAnswer}, and it is not a valid answer. Please enter a valid yes/y no/n answer`);
     // console.log(`User answered ${userAnswer}`)
   }
-  if (!rightAnswer.includes(userAnswer.toLowerCase())) {
-    // console.log(`${userAnswer} is incorrect`);
-    alert(`${userAnswer} is incorrect`);
-  } else {
-    // console.log('Great Guess');
-    alert('Great Guess');
-    score++;
-  };
-  console.log(score);
+  for (let tryNumber = 0; tryNumber < 6; tryNumber ++){
+    if (!rightAnswer.includes(userAnswer.toLowerCase())) {
+      // console.log(`${userAnswer} is incorrect. Please try again.`);
+      prompt(`${userAnswer} is incorrect. Please try again. This is try number ${tryNumber} of six.`);
+    } else {
+      // console.log('Great Guess');
+      alert('Great Guess');
+      score++;
+    }
+  }
 }
+
 
 evalAnswer('Do I have a sister', ['yes', 'y']);
 
@@ -38,4 +58,4 @@ evalAnswer('Should I have skipped lunch?', ['no', 'n']);
 
 //todo: give them afinal message with their name in the alert
 
-alert(`Thanks for playing ${userName}! You were ${score}/5 today, maybe you can do a little better tomorrow?`);
+alert(`Thanks for playing ${userName}! You scored ${score}/5 today, maybe you can do a little better tomorrow?`);
